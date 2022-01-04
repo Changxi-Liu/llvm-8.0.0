10844bfc1d9fc22de2949cd7109745ad11fe09d0
Liu Changxi <changxi_liu@u.nus.edu>
2022-01-04 20:05:35 +0800

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# The LLVM Compiler Infrastructure
+
+This directory and its subdirectories contain source code for LLVM,
+a toolkit for the construction of highly optimized compilers,
+optimizers, and runtime environments.
+# How to install me
+
+- mkdir build&&cd build
+- ../compiling.sh
+- make -j32
+- make install
+
+The header files and library are install into the folder ${LLVM_PROJECT}/install/
---
+++ compiling.sh
@@ -0,0 +1,10 @@
+cmake -G "Unix Makefiles" \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX=../install \
+-DLLVM_TARGETS_TO_BUILD="X86_64" \
+-DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" \
+../llvm
+#-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+#-DCMAKE_BUILD_TYPE=Release \
+#cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" \ -DBUILD_SHARED_LIBS=True -DLLVM_USE_SPLIT_DWARF=True \  -DLLVM_OPTIMIZED_TABLEGEN=True -DLLVM_BUILD_TESTS=False \  -DDEFAULT_SYSROOT="/home/lcx/riscv/_install/riscv32-unknown-elf" \  -DGCC_INSTALL_PREFIX="/home/lcx/riscv/_install" \  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" \  -DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf" \  -DLLVM_TARGETS_TO_BUILD="RISCV" \  -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV" \  -DCMAKE_INSTALL_PREFIX=/home/lcx/_install \  ../llvm
+#/home/lcx/riscv/
